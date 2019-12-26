1187234d93a3bcf5a1ba133eb3f40fa18b40326c
uabhanu <uabhanu@gmail.com>
2019-12-26 14:18:02 +0000

    Project Removed

--- GameEngine/Engine/src/BhanuEngine.h
+++
@@ -1,8 +0,0 @@
-#pragma once //This is for use by Engine Applications only
-
-#include "BhanuEngine/BhanuEngineApp.h"
-#include "BhanuEngine/Log.h"
-
-//----------Entry Point-------------//
-#include "BhanuEngine/EntryPoint.h"
-//----------Entry Point-------------//
