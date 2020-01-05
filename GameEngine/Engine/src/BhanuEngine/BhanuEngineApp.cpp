04e2c82de636377ecf8be31efbc15dd1e96b7f7e
Agasthyeaswar Bhanu Upadhyayula <uabhanu@gmail.com>
2020-01-05 23:39:40 +0000

    Screen colour changed to Blue from Pink Time Pass Window Events | Game Engine series finished

--- GameEngine/Engine/src/BhanuEngine/BhanuEngineApp.cpp
+++ GameEngine/Engine/src/BhanuEngine/BhanuEngineApp.cpp
@@ -34,7 +34,7 @@ namespace BhanuEngine
 	{
 		while(m_IsRunning)
 		{
-			glClearColor(1 , 0 , 1 , 1);
+			glClearColor(0 , 0 , 1 , 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
