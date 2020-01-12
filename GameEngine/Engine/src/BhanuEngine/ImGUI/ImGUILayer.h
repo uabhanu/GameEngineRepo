3063930786ccb2fe072158ad5646250537850e17
Agasthyeaswar Bhanu Upadhyayula <uabhanu@gmail.com>
2020-01-12 17:06:41 +0000

    ImGui Events | Game Engine series finished and everything working fine

--- GameEngine/Engine/src/BhanuEngine/ImGUI/ImGUILayer.h
+++ GameEngine/Engine/src/BhanuEngine/ImGUI/ImGUILayer.h
@@ -3,7 +3,10 @@
 //#include <imgui.h> //Uncomment this if needed
 
 #include "BhanuEngine/BhanuEngineApp.h"
+#include "BhanuEngine/Events/KeyEvent.h"
+#include "BhanuEngine/Events/MouseEvent.h"
 #include "BhanuEngine/Layer.h"
+#include "BhanuEngine/Window.h"
 
 namespace BhanuEngine
 {
@@ -11,6 +14,16 @@ namespace BhanuEngine
 	{
 		private:
 		    float m_Time = 0.0f;
+
+		private:
+		    bool OnKeyPressedEvent(KeyPressedEvent& e);
+			bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+			bool OnKeyTypedEvent(KeyTypedEvent& e);
+		    bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+			bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+			bool OnMouseMovedEvent(MouseMovedEvent& e);
+			bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+			bool OnWindowResizeEvent(WindowResizeEvent& e);
 
 		public:
 			ImGUILayer();
--- GameEngine/Engine/src/Platform/Windows/WindowsWindow.cpp
+++ GameEngine/Engine/src/Platform/Windows/WindowsWindow.cpp
@@ -53,20 +53,17 @@ namespace BhanuEngine
 		SetVSync(true);
 
 		//Set GLFW Callbacks
-		glfwSetWindowSizeCallback(m_Window , [](GLFWwindow* window , int width , int height) //[] is new thing you may need to look into further for better understanding
+		glfwSetCharCallback(m_Window , [](GLFWwindow* window , unsigned int keycode)
 		{
 			WindowData& data = *(WindowData*)(glfwGetWindowUserPointer(window));
-			data.Width = width;
-			data.Height = height;
-
-			WindowResizeEvent event(width , height);
+			KeyTypedEvent event(keycode);
 			data.EventCallback(event);
 		});
 
-		glfwSetWindowCloseCallback(m_Window , [](GLFWwindow* window)
+		glfwSetCursorPosCallback(m_Window , [](GLFWwindow* window , double xPos , double yPos)
 		{
 			WindowData& data = *(WindowData*)(glfwGetWindowUserPointer(window));
-			WindowCloseEvent event; 
+			MouseMovedEvent event((float)xPos , (float)yPos);
 			data.EventCallback(event);
 		});
 
@@ -128,10 +125,20 @@ namespace BhanuEngine
 			data.EventCallback(event);
 		});
 
-		glfwSetCursorPosCallback(m_Window , [](GLFWwindow* window , double xPos , double yPos)
+		glfwSetWindowSizeCallback(m_Window , [](GLFWwindow* window , int width , int height) //[] is new thing you may need to look into further for better understanding
 		{
 			WindowData& data = *(WindowData*)(glfwGetWindowUserPointer(window));
-			MouseMovedEvent event((float)xPos , (float)yPos);
+			data.Width = width;
+			data.Height = height;
+
+			WindowResizeEvent event(width , height);
+			data.EventCallback(event);
+		});
+
+		glfwSetWindowCloseCallback(m_Window , [](GLFWwindow* window)
+		{
+			WindowData& data = *(WindowData*)(glfwGetWindowUserPointer(window));
+			WindowCloseEvent event; 
 			data.EventCallback(event);
 		});
 	}
