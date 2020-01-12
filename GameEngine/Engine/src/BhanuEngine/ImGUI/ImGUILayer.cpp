00b06d278bd7569aa9dcb7aed8be256ddc82f7c6
Agasthyeaswar Bhanu Upadhyayula <uabhanu@gmail.com>
2020-01-12 11:35:01 +0000

    ImGui | Game Engine series 12:25/27:56 and everything working perfectly

---
+++ GameEngine/Engine/src/BhanuEngine/ImGUI/ImGUILayer.cpp
@@ -0,0 +1,34 @@
+#include "EnginePCH.h"
+#include "ImGUILayer.h"
+
+namespace BhanuEngine
+{
+	ImGUILayer::ImGUILayer()
+		: Layer("ImGUILayer")
+	{
+	}
+
+	ImGUILayer::~ImGUILayer()
+	{
+	}
+
+	void ImGUILayer::OnAttach()
+	{
+
+	}
+
+	void ImGUILayer::OnDetach()
+	{
+
+	}
+
+	void ImGUILayer::OnEvent(Event& event)
+	{
+
+	}
+
+	void ImGUILayer::OnUpdate()
+	{
+
+	}
+}
